cea87b3fd745a97d10427dc47c05b7f08c50f608
Mariano Consoni <mariano.consoni@gmail.com>
2026-02-03 12:26:45 -0300

    Hyperliquid run a different branch

--- hyperliquid_strategy.sh
+++ hyperliquid_strategy.sh
@@ -5,7 +5,7 @@ LOG=/root/log/hyperliquid.log
 echo "--------------------------------------------------------------------------" >> $LOG
 echo $(date) >> $LOG
 
-cd /root/defilib >> $LOG 2>&1  && \
-	source venv2/bin/activate >> $LOG 2>&1  && \
+cd /root/defilib && source venv2/bin/activate
+cd /root/defilib_coded_strats >> $LOG 2>&1  && \
 	python3 scripts/backoffice/valuate-and-run.py --no-valuate hyperliquid >> $LOG 2>&1  && \
 	deactivate >> $LOG 2>&1
