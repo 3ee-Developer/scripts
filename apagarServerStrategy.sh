e32ab82356e29df08b64aaeb766830ac4206b2e4
Mariano Consoni <mariano.consoni@gmail.com>
2026-02-11 18:28:55 -0300

    Do not use Uniswap or Shield / Start refactoring

--- apagarServerStrategy.sh
+++ apagarServerStrategy.sh
@@ -5,8 +5,7 @@ source "$(dirname "$0")/shutdown_common.sh"
 
 PROCESS="valuate-and-run"
 INSTANCE_ID="i-09a4ecb38ca786bc5"
-SERVER_NAME="3ee_hyperliquid"
+SERVER_NAME="3ee_strategy"
 
 echo $(date)
-# 15 minutos
 shutdownServer "$PROCESS" "$INSTANCE_ID" "$SERVER_NAME"
--- apagarServerTasks.sh
+++ apagarServerTasks.sh
@@ -8,11 +8,11 @@ INSTANCE_ID="i-086cd22c6d66f5cab"
 SERVER_NAME="3ee_tasks"
 
 # IDs de las instancias que deben estar detenidas antes de apagar
-CHECK_UNISWAP_SERVER="i-042ec3e7fa3906e4b"
-CHECK_HYPERLIQUID_SERVER="i-09a4ecb38ca786bc5"
+##CHECK_UNISWAP_SERVER="i-042ec3e7fa3906e4b"
+##CHECK_HYPERLIQUID_SERVER="i-09a4ecb38ca786bc5"
 
 # ID de API Cosigner
-API_COSIGNER_SERVER="i-01993d9e57b195144"
+##API_COSIGNER_SERVER="i-01993d9e57b195144"
 
 # Función para verificar el estado de una instancia
 check_instance_state() {
@@ -23,24 +23,24 @@ check_instance_state() {
 }
 
 # Esperar hasta que la instancia esté en estado "stopped"
-echo "$(date) - Esperando que la instancia $CHECK_UNISWAP_SERVER esté detenida..."
-while true; do
-    state1=$(check_instance_state "$CHECK_UNISWAP_SERVER")
-    
-    echo "$(date) - Estado de $CHECK_UNISWAP_SERVER: $state1"
-    
-    if [ "$state1" = "stopped" ]; then
-        echo "$(date) - La instancia está detenida. Procediendo a apagar el servidor..."
-        break
-    fi
-    
-    echo "$(date) - Esperando 30 segundos antes de verificar nuevamente..."
-    sleep 30
-done
-
+##echo "$(date) - Esperando que la instancia $CHECK_UNISWAP_SERVER esté detenida..."
+##while true; do
+##    state1=$(check_instance_state "$CHECK_UNISWAP_SERVER")
+##    
+##    echo "$(date) - Estado de $CHECK_UNISWAP_SERVER: $state1"
+##    
+##    if [ "$state1" = "stopped" ]; then
+##        echo "$(date) - La instancia está detenida. Procediendo a apagar el servidor..."
+##        break
+##    fi
+##    
+##    echo "$(date) - Esperando 30 segundos antes de verificar nuevamente..."
+##    sleep 30
+##done
+##
 # Apagar API_COSIGNER_SERVER sin verificar procesos
-echo "$(date) - Apagando API_COSIGNER_SERVER ($API_COSIGNER_SERVER) sin verificar procesos..."
-aws ec2 stop-instances --instance-ids "$API_COSIGNER_SERVER"
+##echo "$(date) - Apagando API_COSIGNER_SERVER ($API_COSIGNER_SERVER) sin verificar procesos..."
+##aws ec2 stop-instances --instance-ids "$API_COSIGNER_SERVER"
 
 echo $(date)
 shutdownServer "$PROCESS" "$INSTANCE_ID" "$SERVER_NAME"
