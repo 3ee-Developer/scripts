6da542138fff6d979f4478e4c6417b91ad695273
Mariano Consoni <mariano.consoni@gmail.com>
2026-02-19 16:11:06 -0300

    fund name changed

--- strategy_crypto_fund.sh
+++ strategy_crypto_fund.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-LOG=/root/log/hyperliquid_evolution.log
+LOG=/root/log/strategy_crypto_fund.log
 
 echo "--------------------------------------------------------------------------" >> $LOG
 echo $(date) >> $LOG
 
 cd /root/defilib >> $LOG 2>&1  && \
 	source venv2/bin/activate >> $LOG 2>&1  && \
-	python3 scripts/backoffice/valuate-and-run.py hyperliquid2 >> $LOG 2>&1  && \
+	python3 scripts/backoffice/valuate-and-run.py strategy_crypto_fund >> $LOG 2>&1  && \
 	deactivate >> $LOG 2>&1
