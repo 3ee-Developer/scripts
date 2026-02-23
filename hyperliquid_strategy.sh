75fc5d0bfbecd0d216dd8b44b7893c1c00eb85e7
Mariano Consoni <mariano.consoni@gmail.com>
2026-02-23 16:46:07 +0000

    Running in different directory

--- hyperliquid_strategy.sh
+++ hyperliquid_strategy.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-LOG=/root/log/hyperliquid.log
+LOG=/root/log/hyperliquid_strategy.log
 
 echo "--------------------------------------------------------------------------" >> $LOG
 echo $(date) >> $LOG
 
 cd /root/defilib && source venv2/bin/activate
 cd /root/defilib_coded_strats >> $LOG 2>&1  && \
+	rm -rf cache/* && \
 	python3 scripts/backoffice/valuate-and-run.py --no-valuate hyperliquid >> $LOG 2>&1  && \
+	python3 scripts/backoffice/upload_gsheet.py >> $LOG 2>&1  && \
 	deactivate >> $LOG 2>&1
