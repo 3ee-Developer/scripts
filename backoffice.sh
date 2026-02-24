1bc297ef1c1f6ea598cd3a6d2d60364d13b2d7bf
Mariano Consoni <mariano.consoni@gmail.com>
2026-02-24 13:48:00 -0300

    fix

--- backoffice.sh
+++ backoffice.sh
@@ -8,6 +8,6 @@ echo $(date) >> $LOG
 cd /root/backoffice >> $LOG 2>&1  && \
 	source venv/bin/activate >> $LOG 2>&1  && \
 	python aws_run.py >> $LOG 2>&1  && \
-	python upload_gsheets_data.py >> $LOG 2>&1  && \
+	python upload_gsheets_data.py --aws >> $LOG 2>&1  && \
 	deactivate >> $LOG 2>&1
 
