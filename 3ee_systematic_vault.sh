42762b3a430dcb7b8f388bf3531d948b57f82340
Mariano Consoni <mariano.consoni@gmail.com>
2026-04-01 17:39:58 -0300

    new script

---
+++ 3ee_systematic_vault.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+LOG=/root/log/3ee_systematic_vault.log
+
+echo "--------------------------------------------------------------------------" >> $LOG
+echo $(date) >> $LOG
+
+cd /root/defilib && source venv2/bin/activate
+cd /root/defilib >> $LOG 2>&1  && \
+	python3 hlq_vault.py >> $LOG 2>&1  && \
+	deactivate >> $LOG 2>&1
